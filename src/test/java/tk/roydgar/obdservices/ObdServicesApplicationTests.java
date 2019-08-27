6306d18a6136b2ec7c2bab03d13fd8faf3a6c43c
Viktor_Gritsaenko <Roydgaryshka@gmail.com>
2019-08-27 12:09:59 +0300

    gcp

--- src/test/java/tk/roydgar/obdservices/ObdServicesApplicationTests.java
+++ src/test/java/tk/roydgar/obdservices/ObdServicesApplicationTests.java
@@ -1,16 +1,7 @@
 package tk.roydgar.obdservices;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@ActiveProfiles("test")
 public class ObdServicesApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
 }
